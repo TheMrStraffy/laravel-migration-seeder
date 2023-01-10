609118e5a1655002ed422e3922648b0d3d7fa2fb
TheMrStraffy <carmelo.miuccio2@gmail.com>
2023-01-10 17:43:20 +0100

    [added] table with all the trains in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,48 @@
 @extends('layouts.main')
 
 @section('content')
-SECTION HERE
+<div class="container">
+
+    <h1 class="text-center">TRAINS FROM ALL OVER THE WOOOOOORLD</h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Company</th>
+            <th scope="col">Departure Station</th>
+            <th scope="col">Arrival Station</th>
+            <th scope="col">Departure Time</th>
+            <th scope="col">Arrival Time</th>
+            <th scope="col">Train Code</th>
+            <th scope="col">Number Of Coaches</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+
+            <tr>
+              <td>{{$train['company']}}</td>
+              <td>{{$train['departure_station']}}</td>
+              <td>{{$train['arrival_station']}}</td>
+              <td>{{$train['departure_time']}}</td>
+              <td>{{$train['arrival_time']}}</td>
+              <td>{{$train['train_code']}}</td>
+              <td>{{$train['coaches_number']}}</td>
+              <td>
+                @if ($train['is_in_time'])
+                    In Time
+                    @elseif (!$train['is_in_time'] && !$train['is_cancelled'])
+                    Delayed
+                    @else
+                    Cancelled
+                @endif
+              </td>
+
+            </tr>
+            @endforeach
+
+        </tbody>
+      </table>
+</div>
+
 @endsection
